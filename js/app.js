2279961a199fde7b9d7af908917dab1b4a532d61
emmaHsiang <122218891@qq.com>
2017-11-14 17:36:21 +0800

    优化上传按钮呈现

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
-var uploadCard = new Vue({
-  el: '.uploadCard',
-  data: {
-    message: 'Hello Vue!'
-  },
-  methods:{
-  	selectFile:function(event){
-  		// alert("selectFile");
-  		$("#file").trigger("click");  
-  	}
-  }
-})
-
+// var uploadCard = new Vue({
+//   el: '.uploadCard',
+//   data: {
+//     message: 'Hello Vue!'
+//   },
+//   methods:{
+//   	selectFile:function(event){
+//   		// alert("selectFile");
+//   		$("#file").trigger("click");  
+//   	}
+//   }
+// })
+ 
 
 
